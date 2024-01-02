548e41c906f335a509c9fceaf392f17fdea8ac29
Pawel Rus <pawelrus.637@gmail.com>
2024-01-03 00:08:39 +0100

    logger added

--- src/main/java/skiresorts/ResortsList.java
+++ src/main/java/skiresorts/ResortsList.java
@@ -1,5 +1,9 @@
 package main.java.skiresorts;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.*;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
@@ -13,7 +17,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ResortsList implements ActionListener {
-	
+    private static Logger logger = LogManager.getLogger(ResortsList.class);
+
 	private JFrame mainFrame = new JFrame("Ski Resorts App");
 	private JPanel titlePanel  = new JPanel();
 	private JPanel listPanel  = new JPanel();
@@ -27,8 +32,10 @@ public class ResortsList implements ActionListener {
 		initFrame();
 		initTitlePanel();
 		initListPanel();
-		mainFrame.setVisible(true);
+		
 		mainFrame.pack();
+		mainFrame.setLocationRelativeTo(null);
+		mainFrame.setVisible(true);
 	}
 	
 	private void initFrame() {
@@ -36,7 +43,7 @@ public class ResortsList implements ActionListener {
 		//mainFrame.setSize(600,600);
 		mainFrame.getContentPane().setBackground(new Color(255,255,204));
 		mainFrame.setLayout(new BorderLayout());
-		mainFrame.setLocationRelativeTo(null);
+		//mainFrame.setLocationRelativeTo(null);
 		
 	}
 	private void initTitlePanel(){
@@ -44,8 +51,8 @@ public class ResortsList implements ActionListener {
 		//titlePanel.setBounds(0,0,600,80);
 		
 		nameField.setBackground(new Color(255,255,204));
-		nameField.setForeground(new Color(25,255,0));
-		nameField.setFont(new Font("Helvetica",Font.BOLD,55));
+		//nameField.setForeground(new Color(25,255,0));
+		nameField.setFont(new Font("Helvetica",Font.BOLD,60));
 		nameField.setHorizontalAlignment(JLabel.CENTER);
 		nameField.setText("Ski Resorts App");
 		nameField.setOpaque(true); //przezroczystosc elementu
@@ -62,7 +69,8 @@ public class ResortsList implements ActionListener {
 		//initialize buttons
 		for(int i=0; i<resorts.length; i++) {
 			buttons[i] = new JButton(resorts[i]);
-			buttons[i].setFont(new Font("Helvetica",Font.BOLD,60));
+			buttons[i].setFont(new Font("Helvetica",Font.BOLD,55));
+			buttons[i].setForeground(new Color (25,255,200));
 			buttons[i].setFocusable(false);
 			buttons[i].addActionListener(this);
 			buttons[i].setBackground(new Color(255, 255, 204));
@@ -73,6 +81,31 @@ public class ResortsList implements ActionListener {
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		int index = 0;
+		for (int i = 0; i < buttons.length; i++) {
+            if (e.getSource() == buttons[i]) {
+                index = i;
+                break;
+            }
+        }
+		switch(index) {
+			case 0:
+				logger.info("Opening Szczyrk Details.");
+				new Szczyrk();
+				break;
+			case 1:
+				new Kotelnica();
+				logger.info("Opening Kotelnica Details.");
+				break;
+			case 2:
+				break;
+			default:
+				break;
+		}
+	}
+	
+	void openResortDetails(String resortName){
+		
 	}
 	
 	public static void main(String[] args) {
