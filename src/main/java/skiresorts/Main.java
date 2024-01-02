2b638b0a5e902ac30aae9588474b9fae5be6d1b2
Pawel Rus <pawelrus.637@gmail.com>
2024-01-02 20:56:50 +0100

    first prototype of main window

---
+++ src/main/java/skiresorts/Main.java
@@ -0,0 +1,10 @@
+package main.java.skiresorts;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		ResortsList resortsList = new ResortsList();
+	}
+
+}
--- src/main/java/skiresorts/ResortsList.java
+++ src/main/java/skiresorts/ResortsList.java
@@ -1,7 +1,80 @@
 package main.java.skiresorts;
 
-public class ResortsList {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class ResortsList implements ActionListener {
+	
+	private JFrame mainFrame = new JFrame("Ski Resorts App");
+	private JPanel titlePanel  = new JPanel();
+	private JPanel listPanel  = new JPanel();
+	private JLabel nameField = new JLabel();
+    private String[] resorts = {"Szczyrk Mountain Resort", "Kotelnica Białczańska", "Resort 3"};
+	JButton[] buttons = new JButton[resorts.length];
+
+	
+	
+	ResortsList(){
+		initFrame();
+		initTitlePanel();
+		initListPanel();
+		mainFrame.setVisible(true);
+		mainFrame.pack();
+	}
+	
+	private void initFrame() {
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//mainFrame.setSize(600,600);
+		mainFrame.getContentPane().setBackground(new Color(255,255,204));
+		mainFrame.setLayout(new BorderLayout());
+		mainFrame.setLocationRelativeTo(null);
+		
+	}
+	private void initTitlePanel(){
+		titlePanel.setLayout(new BorderLayout());
+		//titlePanel.setBounds(0,0,600,80);
+		
+		nameField.setBackground(new Color(255,255,204));
+		nameField.setForeground(new Color(25,255,0));
+		nameField.setFont(new Font("Helvetica",Font.BOLD,55));
+		nameField.setHorizontalAlignment(JLabel.CENTER);
+		nameField.setText("Ski Resorts App");
+		nameField.setOpaque(true); //przezroczystosc elementu
+		
+		titlePanel.add(nameField, BorderLayout.CENTER);
+		mainFrame.add(titlePanel, BorderLayout.NORTH);
+	}
+	
+	private void initListPanel() {
+        listPanel.setLayout(new GridLayout(3,1));
+		//listPanel.setBounds(0,80,600,520);
+		listPanel.setBackground(new Color(255, 255, 204));
+		
+		//initialize buttons
+		for(int i=0; i<resorts.length; i++) {
+			buttons[i] = new JButton(resorts[i]);
+			buttons[i].setFont(new Font("Helvetica",Font.BOLD,60));
+			buttons[i].setFocusable(false);
+			buttons[i].addActionListener(this);
+			buttons[i].setBackground(new Color(255, 255, 204));
+			listPanel.add(buttons[i]);
+		}
+		
+		mainFrame.add(listPanel, BorderLayout.CENTER);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
